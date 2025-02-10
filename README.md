aa53bc16d4a0629c77cbf4397443382348d43707
Maks <maks.znidarsic@gmail.com>
2025-02-10 13:27:43 +0100

    Added the config files...

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # i3
+
+My config for the [i3 window manager](https://i3wm.org).
+
+```
+    pacman -Syu xorg xinit-xorg i3-wm polybar pulseaudio feh ttf-ubuntu-mono-nerd
+```
---
+++ launch.sh
@@ -0,0 +1,11 @@
+
+
+
+#kill existing polybars
+killall -q polybar
+
+#wait until the process has been shutdown
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+#launch polybar
+polybar main -c $HOME/.config/i3/config.ini

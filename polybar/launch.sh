22dc6c54b62ae4e588e158ee0573aa76c7735eb2
Maks <maks.znidarsic@gmail.com>
2026-06-06 00:32:33 +0200

    added qutebrowser clean up scripts

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,5 @@
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-echo main > $HOME/.config/i3/polybar/bar-cache
+echo main > $HOME/.config/i3/polybar/cache
 polybar main -c $HOME/.config/i3/polybar/polybar.ini
--- polybar/swap.sh
+++ polybar/swap.sh
@@ -1,12 +1,12 @@
 
 
 
-cache=$HOME/.config/i3/polybar/bar-cache
+cache=$HOME/.config/i3/polybar/cache
 current=$(cat $cache)
 
 bars=("main" "detail" "off")
 for ((x = 0; x < ${#bars[@]}; x++)); do
-    if [ "${bars[$x]}" = "$current" ]; then
+    if [ ${bars[$x]} = $current ]; then
         echo ${bars[$[ ($x + 1) % ${#bars[@]} ]]} > $cache
     fi
 done
